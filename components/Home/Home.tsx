122bd8973aefd51567bf7abc936c70c71746312c
Silki <129131071+silkiy@users.noreply.github.com>
2025-08-11 09:45:45 +0700

    feat: add Service component and ServiceCard for service offerings display

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero/Hero'
+import Service from './Services/Services'
 
 const Home = () => {
   return (
     <div className='overflow-hidden'>
       <Hero/>
+      <Service/>
     </div>
   )
 }
---
+++ components/Home/Services/Services.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import ServiceCard from "./ServiceCard";
+
+const Service = () => {
+  return (
+    <div className="pt-16 pb-16">
+      <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+        Colleborate with brand <br /> and agencies to create <br /> impactful
+        results
+      </h1>
+      <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-10 mt-20 items-center">
+        <div className="xl:col-start-2">
+          <ServiceCard
+            icon="/images/s2.png"
+            name="Web and Mobile App"
+            description="Designing interfaces that are intuitive, efficient, and enjoyable to use."
+          />
+        </div>
+        <div className="xl:col-start-3">
+          <ServiceCard
+            icon="/images/s4.png"
+            name="Development"
+            description="Designing interfaces that are intuitive, efficient, and enjoyable to use."
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Service;
