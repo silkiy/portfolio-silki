f5804d14f8b50e45daeb6314c03f88d7f6c47856
Silki <129131071+silkiy@users.noreply.github.com>
2025-08-12 19:07:31 +0700

    feat: integrate AOS for animations in Hero component

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -16,12 +16,21 @@ const Hero = () => {
           width={150}
           height={150}
           className="rounded-full border-8 border-[#0c0c48aa]"
+          data-aos="fade-up"
         />
-        <h1 className="text-xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 px-4 text-center font-bold tracking-wide">
+        <h1
+          data-aos="fade-up"
+          data-aos-delay="200"
+          className="text-xl sm:text-4xl md:text-5xl lg:text-6xl mt-6 px-4 text-center font-bold tracking-wide"
+        >
           Hi I&apos;m Silki{" "}
           <span className="text-cyan-200">Fullstack Developer</span>
         </h1>
-        <h2 className="mt-5 text-sm px-2 text-center sm:text-2xl font-medium flex items-center">
+        <h2
+          data-aos="fade-up"
+          data-aos-delay="400"
+          className="mt-5 text-sm px-2 text-center sm:text-2xl font-medium flex items-center"
+        >
           A Passionate
           <span className="text-cyan-200 font-bold inline-block">
             <Typewriter
@@ -40,9 +49,13 @@ const Hero = () => {
             />
           </span>
         </h2>
-        <button className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor-pointer rounded-full text-lg font-medium">
+        <button
+          data-aos="fade-up"
+          data-aos-delay="600"
+          className="mt-6 px-10 py-4 bg-blue-800 hover:bg-blue-900 transition-all duration-300 cursor-pointer rounded-full text-lg font-medium"
+        >
           <span>See my work</span>
-          <BsArrowRight className="w-5 h-5 ml-2 inline-block"/>
+          <BsArrowRight className="w-5 h-5 ml-2 inline-block" />
         </button>
       </div>
     </div>
--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import Hero from './Hero/Hero'
-import Resume from './Resume/Resume'
-import Projects from './Projects/projects'
-import Skills from './Skills/Skills'
-import Contact from './Contact/Contact'
+"use client";
+import React, { useEffect } from "react";
+import Hero from "./Hero/Hero";
+import Resume from "./Resume/Resume";
+import Projects from "./Projects/projects";
+import Skills from "./Skills/Skills";
+import Contact from "./Contact/Contact";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home = () => {
-  return (
-    <div className='overflow-hidden'>
-      <Hero/>
-      <Resume/>
-      <Projects/>
-      <Skills/>
-      <Contact/>
-    </div>
-  )
-}
+  useEffect(() => {
+    const initAOS = async () => {
+      await import("aos");
+      AOS.init({
+        duration: 1000,
+        easing: "ease",
+        once: true,
+        anchorPlacement: "top-bottom",
+      });
+    };
 
-export default Home
+    initAOS();
+  });
+
+  return (
+    <div className="overflow-hidden">
+      <Hero />
+      <Resume />
+      <Projects />
+      <Skills />
+      <Contact />
+    </div>
+  );
+};
+
+export default Home;
