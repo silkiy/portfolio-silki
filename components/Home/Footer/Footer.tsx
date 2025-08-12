acaba1023c8bb3faf8b429ed83f364eb8c706005
Silki <129131071+silkiy@users.noreply.github.com>
2025-08-12 17:48:38 +0700

    feat: add Footer component to display copyright information

---
+++ components/Home/Footer/Footer.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { Fragment, useState, useEffect } from "react";
+
+const Footer: React.FC = () => {
+  const [year, setYear] = useState<number>(new Date().getFullYear());
+
+  useEffect(() => {
+    setYear(new Date().getFullYear());
+  }, []);
+
+  return (
+    <Fragment>
+      <div className="bg-blue-900/20 p-5">
+        <p className="text-lg text-white/70 text-center">{`© ${year}. All Rights Reserved.`}</p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Footer;
