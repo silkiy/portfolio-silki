35134a5ae49fa4b3acc0bb619878027a48cbc529
Silki <129131071+silkiy@users.noreply.github.com>
2025-08-12 17:47:51 +0700

    feat: add Footer and ScrollToTop components to RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ResponsiveNav from "@/components/Home/Navbar/ResponsiveNav";
+import Footer from "@/components/Home/Footer/Footer";
+import ScrollToTop from "@/components/Helper/ScrollToTop";
 
 const font = Inter({
-  weight : ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
@@ -20,10 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${font.className} antialiased bg-[#0d0d1f]`}>
-          <ResponsiveNav />
+      <body className={`${font.className} antialiased bg-[#0d0d1f]`}>
+        <ResponsiveNav />
         {children}
+        <Footer/>
+        <ScrollToTop />
       </body>
     </html>
   );
